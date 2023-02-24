ee2c4a31b60d891d306853328f43d234f519c6c6
tbensheimer <104734721+tbensheimer@users.noreply.github.com>
2023-02-23 21:31:04 -0500

    Create App.js

---
+++ src/App.js
@@ -0,0 +1,15 @@
+import './App.css';
+
+function App() {
+  return (
+<div className='pirate-memory-game'>
+  <h1>Pirate Memory Game</h1>
+  <h3>Turns: </h3>
+  <div className="card-grid">
+    </div>
+  </div>
+
+  );
+}
+
+export default App;
